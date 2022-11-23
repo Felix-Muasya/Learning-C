07814d2f8e5cecc6ea9d799ea3ea0d2b1370e5e8
felix-muasya <66355304+Felix-Muasya@users.noreply.github.com>
2022-11-23 14:22:23 +0300

    Add files via upload

---
+++ Basics/Variables.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    int MyNum = 15;
+    printf("Ndio kusomea C. \nIt's 2:19pm. \nHiashikata Josuke>>>Crazy Diamond. ");
+    // \n\n creates a blank line.
+    return 0;
+}
