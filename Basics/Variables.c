816a85b9d206be1ee4252f41d140398cdb94aea9
felix-muasya <66355304+Felix-Muasya@users.noreply.github.com>
2022-11-23 14:33:31 +0300

    Update Variables.c

--- Basics/Variables.c
+++ Basics/Variables.c
@@ -5,7 +5,7 @@ int main()
     printf("Ndio kusomea C. \nIt's 2:19pm. \nHiashikata Josuke>>>Crazy Diamond. ");
     // \n\n creates a blank line.
     printf("%d", MyNum); //format specifier
-    // format specifiers %d, %i, %c, %f
-    // variable include int, float and char
+    // format specifiers %d, %i, %c, %f, %lf
+    // variable include int, float, double and char
     return 0;
 }
