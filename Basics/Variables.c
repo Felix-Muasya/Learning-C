f48d947a44fadf2251a8a2324737e763179f7ffc
felix-muasya <66355304+Felix-Muasya@users.noreply.github.com>
2022-11-23 14:28:47 +0300

    Update Variables.c

--- Basics/Variables.c
+++ Basics/Variables.c
@@ -4,5 +4,6 @@ int main()
     int MyNum = 15;
     printf("Ndio kusomea C. \nIt's 2:19pm. \nHiashikata Josuke>>>Crazy Diamond. ");
     // \n\n creates a blank line.
+    printf("%d", MyNum); //format specifier
     return 0;
 }
